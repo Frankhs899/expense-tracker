43c5cc8d7f4de237a230f98160be6fa55aa61340
Franklin Hurtado <137368806+Frankhs899@users.noreply.github.com>
2024-09-28 23:58:58 -0500

    Definiendo colores primary y secondary, logica para tema claro y oscuro

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return <div>App</div>;
+  const [theme, setTheme] = useState(() => {
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme) {
+      return storedTheme;
+    }
+
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      return 'dark';
+    }
+
+    return 'light';
+  });
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.querySelector('html').classList.add('dark');
+    } else {
+      document.querySelector('html').classList.remove('dark');
+    }
+  }, [theme]);
+
+  const handleChangeTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+    localStorage.setItem('theme', newTheme);
+  };
+  return (
+    <main className='h-screen flex flex-col items-center justify-center p-4 bg-stone-100 text-stone-950 dark:bg-stone-900 dark:text-stone-50'>
+      <h1>Control de gastos</h1>
+      <button
+        className='bg-primary-500 dark:bg-secondary-500'
+        onClick={handleChangeTheme}
+      >
+        tema
+      </button>
+    </main>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: '#fdd3e7',
+          200: '#fca8ce',
+          300: '#fa7cb6',
+          400: '#f9519d',
+          500: '#f72585',
+          600: '#c61e6a',
+          700: '#941650',
+          800: '#630f35',
+          900: '#31071b',
+        },
+        secondary: {
+          100: '#ccfdf6',
+          200: '#99fbee',
+          300: '#66f9e5',
+          400: '#33f7dd',
+          500: '#00f5d4',
+          600: '#00c4aa',
+          700: '#00937f',
+          800: '#006255',
+          900: '#00312a',
+        },
+      },
+    },
   },
   plugins: [],
 };
