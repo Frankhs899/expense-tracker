b4f1470eec38daf5c49b49f3d0c823aa4391dc5b
Franklin Hurtado <137368806+Frankhs899@users.noreply.github.com>
2024-09-29 00:05:54 -0500

    Definiendo como fuente Poppins

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
     localStorage.setItem('theme', newTheme);
   };
   return (
-    <main className='h-screen flex flex-col items-center justify-center p-4 bg-stone-100 text-stone-950 dark:bg-stone-900 dark:text-stone-50'>
+    <main className='h-screen flex flex-col items-center justify-center p-4 font-[Poppins] bg-stone-100 text-stone-950 dark:bg-stone-900 dark:text-stone-50'>
       <h1>Control de gastos</h1>
       <button
         className='bg-primary-500 dark:bg-secondary-500'
