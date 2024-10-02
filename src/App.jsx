46c9e35794c4d1415f4ed3870b3cef2cc6b3d780
Franklin Hurtado <137368806+Frankhs899@users.noreply.github.com>
2024-10-02 00:55:48 -0500

    Definiendo contexto para temas claro y oscuro

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import { useContext } from 'react';
+import { Context } from './context/GlobalState';
 
 function App() {
-  const [theme, setTheme] = useState(() => {
-    const storedTheme = localStorage.getItem('theme');
-    if (storedTheme) {
-      return storedTheme;
-    }
+  const { theme, handleChangeTheme } = useContext(Context);
 
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      return 'dark';
-    }
-
-    return 'light';
-  });
-
-  useEffect(() => {
-    if (theme === 'dark') {
-      document.querySelector('html').classList.add('dark');
-    } else {
-      document.querySelector('html').classList.remove('dark');
-    }
-  }, [theme]);
-
-  const handleChangeTheme = () => {
-    const newTheme = theme === 'light' ? 'dark' : 'light';
-    setTheme(newTheme);
-    localStorage.setItem('theme', newTheme);
-  };
   return (
-    <main className='h-screen flex flex-col items-center justify-center p-4 font-[Poppins] bg-stone-100 text-stone-950 dark:bg-stone-900 dark:text-stone-50'>
+    <main
+      className={`h-screen flex flex-col items-center justify-center p-4 font-[Poppins] bg-stone-100 text-stone-950 dark:bg-stone-900 dark:text-stone-50 ${
+        theme === 'dark' ? 'dark' : ''
+      }`}
+    >
       <h1>Control de gastos</h1>
       <button
         className='bg-primary-500 dark:bg-secondary-500'
---
+++ src/context/GlobalState.jsx
@@ -0,0 +1,38 @@
+import { createContext, useState, useEffect } from 'react';
+
+// Creando contexto
+export const Context = createContext();
+
+export const GlobalProvider = ({ children }) => {
+  const [theme, setTheme] = useState(() => {
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme) {
+      return storedTheme;
+    }
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      return 'dark';
+    }
+
+    return 'light';
+  });
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.querySelector('html').classList.add('dark');
+    } else {
+      document.querySelector('html').classList.remove('dark');
+    }
+  }, [theme]);
+
+  const handleChangeTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+    localStorage.setItem('theme', newTheme);
+  };
+
+  return (
+    <Context.Provider value={{ theme, handleChangeTheme }}>
+      {children}
+    </Context.Provider>
+  );
+};
